406d92693bafa0a0e15073e4838e1dc5f6585e01
Timothy Ngorima <timothytakudzwa@gmail.com>
2019-01-24 01:52:34 +0200

    My First Commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+import TodoList from './TodoList'
+class App extends Component {
+    state = {  }
+    render() { 
+        return ( <TodoList/> );
+    }
+}
+ 
+export default App;
---
+++ src/TodoItems.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class TodoItems extends Component {
+  
+      render() {
+        var todoEntries = this.props.entries;
+        var listItems = todoEntries.map((item) => {
+            return <li key={item.key}>{item.text}</li>
+        });
+     
+        return (
+          <ul className="theList">
+              {listItems}
+          </ul>
+        );
+    }
+}
+ 
+export default TodoItems;
